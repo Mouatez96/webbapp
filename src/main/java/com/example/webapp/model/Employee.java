84c552c62680a5164e73b221a1fd01942ebb7266
Mouatez96 <mouatezbillah.khalfaoui@hetic.net>
2023-01-11 17:51:19 +0100

    UPDATE model

---
+++ src/main/java/com/example/webapp/model/Employee.java
@@ -0,0 +1,60 @@
+package com.example.webapp.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	
+	private Long id;
+	
+	private String firstName;
+    
+	private String lastName;
+    
+	private String mail;
+	
+	private int age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
